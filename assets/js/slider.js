011baf23e33b21dc48142984ecf9ec22046237e0
celescrei <creimerwork@gmail.com>
2025-07-15 19:40:16 -0300

    Creando Portfolio

---
+++ assets/js/slider.js
@@ -0,0 +1,27 @@
+const track = document.querySelector('.slider-track');
+const slides = document.querySelectorAll('.img-proyect');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let currentIndex = 0;
+
+function updateSlider() {
+  const width = slides[0].clientWidth;
+  track.style.transform = `translateX(-${currentIndex * width}px)`;
+}
+
+next.addEventListener('click', () => {
+  if (currentIndex < slides.length - 1) {
+    currentIndex++;
+    updateSlider();
+  }
+});
+
+prev.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlider();
+  }
+});
+
+window.addEventListener('resize', updateSlider);
